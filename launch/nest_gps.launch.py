31ec9db94c24ff0d238b9fdb630219c60231847b
my name is mostafa <mostafa@iscool.com>
2023-08-09 20:09:30 -0500

    working code for ros2

--- launch/nest_gps.launch.py
+++ launch/nest_gps.launch.py
@@ -16,14 +16,14 @@ def generate_launch_description():
         package="nest_gps",
         executable="nest_gps_node",
         output="screen",
-        parameters=[
-            {"id": LaunchConfiguration("input_id")}
-        ]
+        # parameters=[
+        #     {"id": LaunchConfiguration("input_id")}
+        # ]
     )
 
     # Create the launch description and populate it with the nodes and launch arguments
     ld = LaunchDescription()
-    ld.add_action(input_id_arg)
+    # ld.add_action(input_id_arg)
     ld.add_action(nest_gps_node)
 
     return ld
--- nest_gps/nest_gps_t.py
+++ nest_gps/nest_gps_t.py
@@ -1,10 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from std_srvs.srv import Empty, EmptyResponse
 import gps		# the gpsd interface module
-from std_msgs.msg import Int32
 from sensor_msgs.msg import NavSatFix
-# from msg_pkg.srv import NestGPSMessage, NestGPSMessageResponse
 
 class NestGPS(Node):
 
@@ -12,21 +9,21 @@ class NestGPS(Node):
         super().__init__('n_gps_pub')
         self.nest_gps_pub = self.create_publisher(NavSatFix,'/nest_gps_info',10)
         timer_period = 2.0 #seconds
-        self.timer = self.create_timer(timer_period, self.timer_cb)
+        self.loop_cb()
 
-    def timer_cb(self):
-        rclpy.get_logger().info("/nest_gps_info topic started")
+    def loop_cb(self):
         session = gps.gps(mode=gps.WATCH_ENABLE)
-        nest_gps_data = NavSatFix()
-        # nest_gps_response = NestGPSMessageResponse()
+        self.nest_gps_data = NavSatFix()
+
         try:
-            if 0 == session.read():
+            while 0 == session.read():
                 if not (gps.MODE_SET & session.valid):
-                    print('not gps.MODE_SET & session.valid')
+                    # not useful, probably not a TPV message
+                    continue
 
                 print('Mode: %s(%d) Time: ' %
                     (("Invalid", "NO_FIX", "2D", "3D")[session.fix.mode],
-                        session.fix.mode), end="")
+                    session.fix.mode), end="")
                 # print time, if we have it
                 if gps.TIME_SET & session.valid:
                     print(session.fix.time, end="")
@@ -35,26 +32,28 @@ class NestGPS(Node):
 
                 if ((gps.isfinite(session.fix.latitude) and
                     gps.isfinite(session.fix.longitude))):
-                    print(" Lat %.6f Lon %.6f Alt %6f " %
-                        (session.fix.latitude, session.fix.longitude, session.fix.altitude))
-                    nest_gps_data.latitude = session.fix.latitude
-                    nest_gps_data.longitude = session.fix.longitude
-                    nest_gps_data.altitude = session.fix.altitude
-                    self.nest_gps_pub.publish(nest_gps_data)
+                    print(" Lat %.6f Lon %.6f" %
+                        (session.fix.latitude, session.fix.longitude))
+                    self.nest_gps_data.latitude = session.fix.latitude
+                    self.nest_gps_data.longitude = session.fix.longitude
+                    self.nest_gps_data.altitude = session.fix.altitude
+                    self.nest_gps_pub.publish(self.nest_gps_data)
                 else:
-                    print(" Lat n/a Lon n/a Alt n/a")
+                    print(" Lat n/a Lon n/a")
 
-        except KeyboardInterrupt:  # ^C
-            print('/nest_gps service stopped')
+        except KeyboardInterrupt:
+            print('nest_gps interrupted')
 
-        # session.close()
+        # Got ^C, or fell out of the loop.  Cleanup, and leave.
+        session.close()
+        exit(0)
 
 def main(args=None):
-    print("nest_gps node started!")
+    print("nest_gps node started")
     rclpy.init(args=args)
-    nest_gps_pub = NestGPS()
-    rclpy.spin(nest_gps_pub)
-    nest_gps_pub.destroy_node()
+    nest_gps = NestGPS()
+    rclpy.spin(nest_gps)
+    nest_gps.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
